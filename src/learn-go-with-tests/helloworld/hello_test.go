42d44e491acbbacaa48305d426ee45a9f5e6ae60
ito0804takuya <ito0804takuya@gmail.com>
2024-01-24 22:21:35 +0900

    Hello関数に引数で注入できるよう拡張

--- src/learn-go-with-tests/helloworld/hello_test.go
+++ src/learn-go-with-tests/helloworld/hello_test.go
@@ -5,8 +5,8 @@ import "testing"
 // ファイル名は _test.go である必要がある
 // テスト関数名は Test で始まる必要がある
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello World"
+	got := Hello("John")
+	want := "Hello, John"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
