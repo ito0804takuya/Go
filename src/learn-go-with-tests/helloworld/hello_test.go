8c8846335cdef50a93e6254030cdd663f59a2b5a
ito0804takuya <ito0804takuya@gmail.com>
2024-01-25 07:11:32 +0900

    空文字の場合はHello World

--- src/learn-go-with-tests/helloworld/hello_test.go
+++ src/learn-go-with-tests/helloworld/hello_test.go
@@ -5,10 +5,22 @@ import "testing"
 // ファイル名は _test.go である必要がある
 // テスト関数名は Test で始まる必要がある
 func TestHello(t *testing.T) {
-	got := Hello("John")
-	want := "Hello, John"
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("John")
+		want := "Hello, John"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say hello world when empty string is supplied", func(t *testing.T) {
+		got := Hello("") // 空文字
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
