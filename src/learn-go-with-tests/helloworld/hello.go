dc566af02f689a02c62ef788e87c12a770b6aae0
ito0804takuya <ito0804takuya@gmail.com>
2024-01-25 08:34:15 +0900

    関数に切り出し

--- src/learn-go-with-tests/helloworld/hello.go
+++ src/learn-go-with-tests/helloworld/hello.go
@@ -14,16 +14,21 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+// 返り値の定義でprefix変数を定義できる！ (Named return value)
+// それで、それをそのまま関数内で使えるのはもちろん、return とするだけで、prefix変数が返される！
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case japanese:
 		prefix = japaneseHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
-}
+	return
 }
 
 func main() {
