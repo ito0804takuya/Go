d74cfbf9862969d50adadbeeebdae4394f4b674d
ito0804takuya <ito0804takuya@gmail.com>
2024-01-27 21:18:12 +0900

    四角形の計算

---
+++ src/learn-go-with-tests/struct/shapes.go
@@ -0,0 +1,11 @@
+package main
+
+// 四角形の周長
+func Perimeter(width, height float64) float64 {
+	return 2 * (width + height)
+}
+
+// 四角形の面積
+func Area(width, height float64) float64 {
+	return width * height
+}
