887ca4a8cc49aedd7d9c872f4c2a014de41bac0e
ito0804takuya <ito0804takuya@gmail.com>
2024-01-27 21:26:37 +0900

    四角形を構造体へ

--- src/learn-go-with-tests/struct/shapes.go
+++ src/learn-go-with-tests/struct/shapes.go
@@ -1,11 +1,17 @@
 package main
 
+// 四角形
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
 // 四角形の周長
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // 四角形の面積
-func Area(width, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
