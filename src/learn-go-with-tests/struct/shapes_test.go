f7bd7f4f1544064cf5c65846924a67bc0a6a5e91
ito0804takuya <ito0804takuya@gmail.com>
2024-01-28 07:45:34 +0900

    テーブル駆動テストにてfailした場合にデバッグ情報が出るよう、各テストの名前追加し、t.Runで個別実行

--- src/learn-go-with-tests/struct/shapes_test.go
+++ src/learn-go-with-tests/struct/shapes_test.go
@@ -17,19 +17,22 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	// テーブル駆動テスト : 同じ方法でテストできるテストケースのリストを作ってテストする
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
 		// テストケース
-		{shape: Rectangle{12, 6}, want: 72.0},
-		{shape: Circle{10}, want: 314.1592653589793},
-		{shape: Triangle{12, 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, want: 72.0},
+		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g want %g", got, tt.want)
+			}
+		})
 	}
 }
