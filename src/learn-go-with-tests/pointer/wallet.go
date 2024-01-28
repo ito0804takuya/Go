d66bc2352ce3a26f6152b3881f720c767e2cedb5
ito0804takuya <ito0804takuya@gmail.com>
2024-01-28 17:11:12 +0900

    口座はビットコインの口座だった

--- src/learn-go-with-tests/pointer/wallet.go
+++ src/learn-go-with-tests/pointer/wallet.go
@@ -1,21 +1,21 @@
 package main
 
-import "fmt"
+type Bitcoin int
 
 // 口座
 type Wallet struct {
-	balance int // 残高
+	balance Bitcoin // 残高
 }
 
 // 入金
 // func (w Wallet) だと、w はWallet構造体の値のコピーなので、元の値が更新されない。
 // func (w *Wallet) として、Wallet構造体のポインターとして読み取る
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // 残高
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	// return (*w).balance と等価 (*演算子 : ポインターを参照して、（ポインターが示す）格納先のオブジェクトへアクセスする。)
 	return w.balance
 }
--- src/learn-go-with-tests/pointer/wallet_test.go
+++ src/learn-go-with-tests/pointer/wallet_test.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(10) // 入金
+	wallet.Deposit(Bitcoin(10)) // 入金
 
 	got := wallet.Balance() // 残高
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
