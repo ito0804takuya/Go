9f7c6e52af53b2f0f4a585c5daabe930abd72b58
ito0804takuya <ito0804takuya@gmail.com>
2024-01-28 17:23:52 +0900

    テストをリファクタリング

--- src/learn-go-with-tests/pointer/wallet_test.go
+++ src/learn-go-with-tests/pointer/wallet_test.go
@@ -6,27 +6,24 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(10)) // 入金
-	
-		got := wallet.Balance() // 残高
-		want := Bitcoin(10)
-	
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10)) // 入金
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(15))
-
-		got := wallet.Balance()
-		want := Bitcoin(5)
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(5))
 	})
 }
