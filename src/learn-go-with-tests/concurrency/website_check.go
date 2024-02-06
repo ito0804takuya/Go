4165f2c4ccb8a2f46c6c8827566e416bad31943d
ito0804takuya <ito0804takuya@gmail.com>
2024-02-06 21:45:39 +0900

    ゴルーチンを使って高速化

--- src/learn-go-with-tests/concurrency/website_check.go
+++ src/learn-go-with-tests/concurrency/website_check.go
@@ -1,13 +1,28 @@
 package concurrency
 
 type WebsiteChecker func(string) bool
+type result struct {
+	// フィールド名をつけるのが難しいときやつけなくていいとき、型名の定義だけで、型名をそのまま使える
+	string
+	bool
+}
 
 // WebsiteChecker関数をDIする
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		// ゴルーチンを使うときは、関数呼び出しの形にする必要があるので、無名関数をしばしば使う
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
+	
 	return results
 }
