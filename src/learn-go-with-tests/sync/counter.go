b671512c5d900c0c76462e3c3aa3256eaf03950f
ito0804takuya <ito0804takuya@gmail.com>
2024-02-11 11:05:52 +0900

    並列に使用できるよう修正

--- src/learn-go-with-tests/sync/counter.go
+++ src/learn-go-with-tests/sync/counter.go
@@ -1,11 +1,17 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	mu sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
-	c.value += 1
+	// Incを呼び出すときにロックをかける
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
 }
 
 func (c *Counter) Value() int {
