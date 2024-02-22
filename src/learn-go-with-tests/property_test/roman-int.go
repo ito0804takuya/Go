9959fd5384c8ca3de9c7eb6cf4fffa2fb490f098
ito0804takuya <ito0804takuya@gmail.com>
2024-02-22 21:38:39 +0900

    汚いが、4の場合を通す

--- src/learn-go-with-tests/property_test/roman-int.go
+++ src/learn-go-with-tests/property_test/roman-int.go
@@ -6,7 +6,11 @@ func ConvertToRoman(arabic int) string {
 	// 文字列を構築するのに効率的なstrings.Builderを使う
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		// バッファに溜め込む
 		result.WriteString("I")
 	}
