ccef4e068dfb91b4af4c9d15cb6d29007cefbbfb
ito0804takuya <ito0804takuya@gmail.com>
2024-02-22 21:29:28 +0900

    テーブルベースのテストへ

--- src/learn-go-with-tests/property_test/roman-int_test.go
+++ src/learn-go-with-tests/property_test/roman-int_test.go
@@ -3,22 +3,22 @@ package property_test
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Description string
+		Input int
+		Want string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+	}
 
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			output := ConvertToRoman(test.Input)
+			if output != test.Want {
+				t.Errorf("got %q, want %q", output, test.Want)
+			}
+		})
+	}
 }
