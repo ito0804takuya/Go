732df8631c8da62517c5adf3eb15809fe31e7e20
ito0804takuya <ito0804takuya@gmail.com>
2024-02-24 09:43:00 +0900

    テストからdescriptionを排除

--- src/learn-go-with-tests/property_test/roman-int_test.go
+++ src/learn-go-with-tests/property_test/roman-int_test.go
@@ -1,31 +1,33 @@
 package property_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
 		Input int
 		Want string
 	}{
-		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV", 4, "IV"},
-		{"5 gets converted to V", 5, "V"},
-		{"6 gets converted to VI", 6, "VI"},
-		{"7 gets converted to VII", 7, "VII"},
-		{"8 gets converted to VIII", 8, "VIII"},
-		{"9 gets converted to IX", 9, "IX"},
-		{"10 gets converted to X", 10, "X"},
-		{"40 gets converted to XL", 40, "XL"},
-		{"47 gets converted to XLVII", 47, "XLVII"},
-		{"49 gets converted to XLIX", 49, "XLIX"},
-		{"50 gets converted to L", 50, "L"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Input, test.Want), func(t *testing.T) {
 			output := ConvertToRoman(test.Input)
 			if output != test.Want {
 				t.Errorf("got %q, want %q", output, test.Want)
