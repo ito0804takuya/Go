ef96fe564540819127fa725e2e23f64262c7324b
ito0804takuya <ito0804takuya@gmail.com>
2024-02-24 09:18:39 +0900

    リファクタリング オブジェクト指向で

--- src/learn-go-with-tests/property_test/roman-int.go
+++ src/learn-go-with-tests/property_test/roman-int.go
@@ -2,30 +2,28 @@ package property_test
 
 import "strings"
 
+type RomanNumeral struct {
+	Value int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral {
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
-	// 文字列を構築するのに効率的なstrings.Builderを使う
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic >= 10:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic == 9:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic >= 5:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic == 4:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, num := range allRomanNumerals {
+		for arabic >= num.Value {
+			result.WriteString(num.Symbol)
+			arabic -= num.Value
 		}
 	}
 
-	// バッファを文字列におこして返す
 	return result.String()
 }
