6bbcd7b44239064f13f1aeb05645e4eea0990227
ito0804takuya <ito0804takuya@gmail.com>
2024-02-25 10:47:25 +0900

    別のユーザーのテストを追加

--- src/learn-go-with-tests/http-server/server.go
+++ src/learn-go-with-tests/http-server/server.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // プレイヤーが勝ったゲームの数を追跡できるWebサーバー
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	} else if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }
--- src/learn-go-with-tests/http-server/server_test.go
+++ src/learn-go-with-tests/http-server/server_test.go
@@ -23,4 +23,18 @@ func TestGETPlayers(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
+
+	t.Run("Floydのスコアを取得", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
