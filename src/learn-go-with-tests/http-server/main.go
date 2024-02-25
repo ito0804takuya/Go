f10e32e5cc4b91a9e73dd6dd389864c8cf1b95aa
ito0804takuya <ito0804takuya@gmail.com>
2024-02-25 10:57:17 +0900

    main関数を作成

---
+++ src/learn-go-with-tests/http-server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// http.HandlerFunc : func(w http.ResponseWriter, r *http.Request)という形の普通の関数を、
+	//                    http.HandlerFunc型に変換することでhandlerが得られる。
+	// handler : ServeHTTP というメソッドを実装しているもの。
+	//           （http.HandlerFunc というinterfaceを実装しているもの。）
+	handler := http.HandlerFunc(PlayerServer)
+
+	// http.ListenAndServe : サーバーを起動
+	log.Fatal(http.ListenAndServe(":5000", handler))
+}
