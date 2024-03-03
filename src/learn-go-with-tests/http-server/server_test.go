c9b1628397c8ae3097b227f364f02a5a997782aa
ito0804takuya <ito0804takuya@gmail.com>
2024-03-04 08:03:27 +0900

    /leagueを追加

--- src/learn-go-with-tests/http-server/server_test.go
+++ src/learn-go-with-tests/http-server/server_test.go
@@ -115,3 +115,18 @@ func newPostWinRequest(name string) *http.Request {
 	request, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
 	return request
 }
+
+// 保存されている全てのプレイヤーリストをJSONで取得する/leagueのテスト
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &PlayerServer{&store}
+
+	t.Run("it return 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
